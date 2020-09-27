571d6917018d34d285beb5bc27520804a8d6bce3
mohanbabu2706 <71622856+mohanbabu2706@users.noreply.github.com>
2020-09-27 20:09:16 +0530

    Add files via upload

---
+++ 27-Sept-2020/number.py
@@ -0,0 +1,10 @@
+#create 2 new lists
+height = [1,87, 1.87, 1.82, 1.91, 1.90, 1.85]
+weight = [81.65, 97.52, 95.25, 92.98, 86.18, 88.45]
+
+#import the numpy package as np
+import numpy as np
+
+#create 2 numpy arrays
+np_height = np.array(height)
+np_weight = np.array(weight)
