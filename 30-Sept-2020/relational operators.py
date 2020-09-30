ce1524675ce746577281cb55f59681b0cdff710e
mohanbabu2706 <71622856+mohanbabu2706@users.noreply.github.com>
2020-09-30 10:41:46 +0530

    Add files via upload

---
+++ 30-Sept-2020/relational operators.py
@@ -0,0 +1,26 @@
+num1 = 10
+num2 = 5
+if(num1 == num2):
+    print("True")
+else:
+    print("False")
+if(num1 != num2):
+    print("True")
+else:
+    print("False")
+if(num1 > num2):
+    print("True")
+else:
+    print("False")
+if(num1 < num2):
+    print("Line4-True condition")
+else:
+    print("Line4-False condition")
+if(num1 >= num2):
+    print("Line5-True condition")
+else:
+    print("Line5-False condition")
+if(num1 <= num2):
+    print("Line6-True condition")
+else:
+    print("Line6-false condition")
---
+++ 30-Sept-2020/square.py
@@ -0,0 +1,9 @@
+while true:
+    print("enter -1 for exit.")
+    num=int(input("Enter a numbeL:"))
+    if num==-1:
+        break
+    else:
+        number=float(num):
+        number_sqrt=number**0.5
+print("square root of %0.2f is %0.2f"%(number,number-sqrt))
